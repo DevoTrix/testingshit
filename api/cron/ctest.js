aba2dd30154fbb1f25ab0b75d5cd1c19e6c80c02
Joseph Chang <71916304+DevoTrix@users.noreply.github.com>
2024-03-23 16:06:36 -0700

    dsda

--- api/cron/ctest.js
+++ api/cron/ctest.js
@@ -3,7 +3,7 @@ const app = express();
 
 console.log("Entered ctest.js");
 
-app.get("/",  async (req, res) => {
+app.get("/api/cron",  async (req, res) => {
     console.log("Hello World - Cron here!");
     res.sendStatus(200);
   });
